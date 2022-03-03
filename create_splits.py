b90ccad8fc1aa94b29fb70a72e10c2f68022df2a
sdassi <48656725+sdassi@users.noreply.github.com>
2022-03-04 00:41:01 +0100

    add script for splitting data

--- create_splits.py
+++ create_splits.py
@@ -1,11 +1,27 @@
 import argparse
 import glob
 import os
-import random
-
-import numpy as np
+from random import shuffle
+from shutil import move
 
 from utils import get_module_logger
+
+TRAIN_DIR = "train"
+VAL_DIR = "val"
+TEST_DIR = "test"
+TRAIN_VAL_TEST_PROP = [0.8, 0.1, 0.1]
+
+
+def make_directories(base_dir):
+    train_dir = os.path.join(base_dir, TRAIN_DIR)
+    val_dir = os.path.join(base_dir, VAL_DIR)
+    test_dir = os.path.join(base_dir, TEST_DIR)
+
+    os.makedirs(train_dir, exist_ok=True)
+    os.makedirs(val_dir, exist_ok=True)
+    os.makedirs(test_dir, exist_ok=True)
+
+    return train_dir, val_dir, test_dir
 
 
 def split(source, destination):
@@ -17,7 +33,19 @@ def split(source, destination):
         - source [str]: source data directory, contains the processed tf records
         - destination [str]: destination data directory, contains 3 sub folders: train / val / test
     """
-    # TODO: Implement function
+    train_dir, val_dir, test_dir = make_directories(destination)
+    tfrec_files = glob.glob(os.path.join(source, "*tfrecord"))
+    shuffle(tfrec_files)
+    threshold_train = round(len(tfrec_files) * TRAIN_VAL_TEST_PROP[0])
+    for tfrec_file in tfrec_files[:threshold_train]:
+        move(tfrec_file, train_dir)
+
+    threshold_val = round(len(tfrec_files) * TRAIN_VAL_TEST_PROP[1])
+    for tfrec_file in tfrec_files[threshold_train:threshold_train+threshold_val]:
+        move(tfrec_file, val_dir)
+
+    for tfrec_file in tfrec_files[threshold_train+threshold_val:]:
+        move(tfrec_file, test_dir)
 
 
 if __name__ == "__main__":
