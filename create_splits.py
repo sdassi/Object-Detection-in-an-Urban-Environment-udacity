ad9c82eb72179eb7bae0fcca2738c7629c3a4843
sdassi <48656725+sdassi@users.noreply.github.com>
2022-03-14 23:06:27 +0100

    reformat py files

--- create_splits.py
+++ create_splits.py
@@ -41,21 +41,23 @@ def split(source, destination):
         move(tfrec_file, train_dir)
 
     threshold_val = round(len(tfrec_files) * TRAIN_VAL_TEST_PROP[1])
-    for tfrec_file in tfrec_files[threshold_train:threshold_train+threshold_val]:
+    for tfrec_file in tfrec_files[threshold_train : threshold_train + threshold_val]:
         move(tfrec_file, val_dir)
 
-    for tfrec_file in tfrec_files[threshold_train+threshold_val:]:
+    for tfrec_file in tfrec_files[threshold_train + threshold_val :]:
         move(tfrec_file, test_dir)
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Split data into training / validation / testing')
-    parser.add_argument('--source', required=True,
-                        help='source data directory')
-    parser.add_argument('--destination', required=True,
-                        help='destination data directory')
+    parser = argparse.ArgumentParser(
+        description="Split data into training / validation / testing"
+    )
+    parser.add_argument("--source", required=True, help="source data directory")
+    parser.add_argument(
+        "--destination", required=True, help="destination data directory"
+    )
     args = parser.parse_args()
 
     logger = get_module_logger(__name__)
-    logger.info('Creating splits...')
+    logger.info("Creating splits...")
     split(args.source, args.destination)
